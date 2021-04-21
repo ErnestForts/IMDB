9dc1d8b7ddccd187d1ed86ce883c9a31271dbd22
ErnestForts <ernest.forts@eram.cat>
2021-04-21 16:49:17 +0200

    subo clase

---
+++ clases.ts
@@ -0,0 +1,30 @@
+class Professional{
+    public name : string
+    public age : number
+    public genere : string
+    public weight : number
+    public height : number
+    public hairColor : string
+    public eyeColor : string
+    public race : string
+    public isRetired : boolean
+    public nationality : string
+    public oscarsNumber : number
+    public profession : string
+
+    constructor(name : string, age : number, genere : string, weight : number, height : number, hairColor : string, eyeColor : string, race : string, isRetired : boolean, nationality : string, oscarsNumber : number, profession : string){
+        this.name = name
+        this.age = age
+        this.genere = genere
+        this.weight = weight
+        this.height = height
+        this.hairColor = hairColor
+        this.eyeColor = eyeColor
+        this.race = race
+        this.isRetired = isRetired
+        this.nationality = nationality
+        this.oscarsNumber = oscarsNumber
+        this.profession = profession
+    }
+
+}
